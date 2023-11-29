4ff1c8e6f84c34efd13dfcc6d6c752f259c5e91d
~Erick <143459790+erickfesantos@users.noreply.github.com>
2023-11-29 11:46:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 let botao = document.querySelector("#botao");
+let P2 = document.querySelector("#P2");
+
+let recebeClasse;
 
 class anotacao{
-  constructor(cont){
-    conteudo=cont;
+  constructor(conteudo){
+    this.conteudo=conteudo;
   }
   
 }
 
-botao.addEventListener("click", ()=>{alert("Funfou")});
+ function criarAnotacao(texto){
+    let div = document.createElement("div");
+    div.setAttribute("class","caixasAnotacoes");
+    div.textContent = texto;
+    return div;
+  }
+  
+
+botao.addEventListener("click", ()=>{
+  cont = document.querySelector("#texto").value;
+  recebeClasse = new anotacao(cont);
+  
+  P2.appendChild(criarAnotacao(recebeClasse.conteudo));
+ 
+  
+});
