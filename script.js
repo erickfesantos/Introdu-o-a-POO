d3fbb84fd49c9310975b33f96c6e730b7f5493b8
~Erick <143459790+erickfesantos@users.noreply.github.com>
2023-12-05 19:48:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 let botao = document.querySelector("#botao");
 let P2 = document.querySelector("#P2");
 
-let recebeClasse;
+let recebeClasse, tipoDeLembrete;
 
 class anotacao{
   constructor(conteudo){
     this.conteudo=conteudo;
   }
-  
 }
 
- function criarAnotacao(texto){
+class anotacaoTipo extends anotacao{
+  constructor(conteudo, tipo){
+    super(conteudo);
+    this.tipo=tipo;
+  }
+}
+
+
+ function criarAnotacao(texto, tipo){
     let div = document.createElement("div");
     div.setAttribute("class","caixasAnotacoes");
-    div.textContent = texto;
+    div.textContent = `Tipo: ${tipo} - ${texto}`;
+    
+    
     return div;
   }
   
 
 botao.addEventListener("click", ()=>{
   cont = document.querySelector("#texto").value;
-  recebeClasse = new anotacao(cont);
+  tipoDeLembrete = prompt("Informe o tipo de lembrete");
   
-  P2.appendChild(criarAnotacao(recebeClasse.conteudo));
+  recebeClasse = new anotacaoTipo(cont, tipoDeLembrete);
+  
+  P2.appendChild(criarAnotacao(recebeClasse.conteudo, recebeClasse.tipo));
  
   
 });
